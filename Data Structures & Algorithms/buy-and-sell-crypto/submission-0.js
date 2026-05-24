44c1908f0133ef520ae321d7006a753befa01600
Sahil-09 <65083346+Sahil-09@users.noreply.github.com>
2026-05-24 21:48:03 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let profit=0
+        for(let i=0;i<prices.length;i++){
+            for(let j=i+1;j<prices.length;j++){
+                let diff = prices[j]-prices[i]
+                profit = Math.max(profit,diff)
+            }
+        }
+        return profit
+    }
+}
