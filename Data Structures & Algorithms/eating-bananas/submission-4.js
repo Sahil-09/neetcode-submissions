921ef92949774b261bc093adde4863624f575176
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-06-29 14:44:20 +0530

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        piles.sort((a,b)=>a-b)
+        let l = 1
+        let m = piles[piles.length-1]
+        while(l<=m){
+            let mid = Math.floor((l+m)/2)
+            let mh=0
+            for(let i=0;i<piles.length;i++){
+                mh+=Math.ceil(piles[i]/mid)
+            }
+            if(mh<=h){
+                m=mid-1
+            }else{
+                l=mid+1
+            }
+        }
+        return l
+    }
+}
