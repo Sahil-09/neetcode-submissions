cb734c04e9dccdc73d001fd07c6f0ea1eb06d2ae
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-07-05 19:44:03 +0530

    Add: evaluate-reverse-polish-notation - submission-6

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-6.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        let stack=[]
+        if(tokens.length==1){
+            return tokens[0]
+        }
+        let map = {
+            '+':(a,b)=>Number(a)+Number(b),
+            '-':(a,b)=>Number(a)-Number(b),
+            '*':(a,b)=>Number(a)*Number(b),
+            '/':(a,b)=>Math.trunc(Number(a)/Number(b)),
+        }
+        let result
+        tokens.forEach((el)=>{
+            if(map[el]){
+               let first = stack.pop()
+               let second = stack.pop()
+               result =  map[el](second,first) 
+               stack.push(result)
+            }else{
+                stack.push(el)
+            }
+        })
+        return result
+    }
+}
