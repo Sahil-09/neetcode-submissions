c3fa8c95f1fde5367ed157ed637ac0e03bbb5059
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-07-22 22:49:25 +0530

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        let res=[]
+        let subSetArr = []
+        let numMap={}
+        let ansMap={}
+        nums.forEach((el)=>{
+            numMap[el] = true
+        })
+        function dfs(i,val){
+            if(i>=nums.length || val>=target){
+                let elm = subSetArr.join('')
+                if(val==target && !ansMap[elm]){
+                    res.push(subSetArr.slice())
+                    ansMap[elm] = true
+                }
+                return
+            }
+            subSetArr.push(nums[i])
+            val+=nums[i]
+            dfs(i+1,val)
+            if(val<=target){
+                dfs(i,val)
+            }
+            val-=nums[i]
+            subSetArr.pop()
+            if(val<=target){
+                dfs(i+1,val)
+            }
+        }
+        dfs(0,0)
+        return res
+    }
+}
