8c36f409187f47efacc0fbd4732ff1f39d48cde5
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-07-29 03:40:07 +0530

    Add: path-sum - submission-1

---
+++ Data Structures & Algorithms/path-sum/submission-1.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} targetSum
+     * @return {boolean}
+     */
+    hasPathSum(root, targetSum) {
+        function dfs(node,sum){
+            if(!node){
+                return false
+            }
+            sum+=node.val
+            if(!node.left && !node.right){
+                return sum==targetSum
+            }
+            let left = dfs(node.left,sum)
+            let right=false
+            if(!left){
+             right = dfs(node.right,sum)
+            }
+            return left || right
+        }
+        return dfs(root,0)
+    }
+}
