372985186762ee1c7315672cc562a2420c14e70e
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-08-16 21:05:52 +0530

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        stones.sort((a,b)=>a-b)
+        while(stones.length>1){
+            let a = stones.pop()
+            let b = stones.pop()
+            if(a==b){
+                continue;
+            }
+            if(a>b){
+                stones.push(a-b)
+            }else{
+                stones.push(b-a)
+            }
+            stones.sort((a,b)=>a-b)
+        }
+        return stones[0] || 0
+    }
+}
