30bfae4910c745f56bd3cf178822cf11deb11a05
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-08-16 20:54:34 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.js
@@ -0,0 +1,23 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.k=k
+        this.heap=nums
+    }
+
+    
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.heap.push(val)
+        this.heap.sort((a,b)=>b-a)
+        return this.heap[this.k-1]
+    }
+
+}
