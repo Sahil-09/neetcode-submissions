d6b99231e9960fd0b9de34b9e5512240a7238883
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-08-18 19:36:03 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        let prev2 = cost[0];
+  let prev1 = cost[1];
+
+  for (let i = 2; i < cost.length; i++) {
+    const current = cost[i] + Math.min(prev1, prev2);
+    prev2 = prev1;
+    prev1 = current;
+  }
+
+  return Math.min(prev1, prev2);
+    }
+}
