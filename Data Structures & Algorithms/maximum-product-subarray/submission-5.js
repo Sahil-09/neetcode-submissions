56feea27e06ad877409f3c6331b73d9596299bf0
Sahil Patel <65083346+Sahil-09@users.noreply.github.com>
2026-08-28 20:01:58 +0530

    Add: maximum-product-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxProduct(nums) {
+        let res = nums.slice().sort((a, b) => b - a)[0];
+        console.log(res);
+        let currMin = 1,
+            currMax = 1;
+        for (let n of nums) {
+            if (n == 0) {
+                ((currMin = 1), (currMax = 1));
+                continue;
+            }
+            let tmp = currMax * n;
+            currMax = Math.max(tmp, n * currMin, n);
+            currMin = Math.min(tmp, n * currMin, n);
+            res = Math.max(res, currMax);
+        }
+        return res;
+    }
+}
